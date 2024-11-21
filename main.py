f0aa011aa4d572c9d9eb0c0c97c44b6e04505b75
kruglovasm <115586869+kruglovasm@users.noreply.github.com>
2024-11-21 15:58:16 +0300

    Update main.py

--- main.py
+++ main.py
@@ -8,7 +8,7 @@ text_request = """Столбцы: Название офиса, Адрес офи
 БЦ «Лотте Плаза» - 121099, Москва, Новинский бульвар, 8с1. 
 Центр робототехники - 119501, Москва, ул. Лобачевского, 130Г."""
 
-access_token = "t1.9euelZqdkZqXkpLNlMzKio_MlIrJje3rnpWazcrMxp7Pi4-KlJmMnJnInsvl8_cOFnpH-e8_WQk8_N3z905Ed0f57z9ZCTz8zef1656VmsyTjprHkYnHj4vPycfOls2N7_zF656VmsyTjprHkYnHj4vPycfOls2N.MpoMHI76kKiyhdCgqeKFabhM9B24kwJ1_fIO4U8lt1kK9hHIf4UMkDu4XtQF_sZLymGBbSdvutz9klSZ-NP7Cg"
+access_token = "your_access_token"
 catalog_id = "b1g0or8o57qjta3hncoe"
 
 output_file = "bubu_output.csv"
